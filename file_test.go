3425f401d72ea402f61153d77ea5d18d6e416bd5
hiscaler <hiscaler@gmail.com>
2019-08-13 15:06:08 +0800

    ...

--- file_test.go
+++ file_test.go
@@ -1,11 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestIsDir(t *testing.T) {
-	if IsDir("D:\\") {
-		t.Log("Successful")
-	} else {
-		t.Error("Failed")
+	dirs := []string{
+		"C:\\",
+		"D:\\",
+	}
+	files := []string{
+		"C:\\a.txt",
+		"D:\\b.txt",
+	}
+	for _, dir := range dirs {
+		if !IsDir(dir) {
+			t.Errorf("`%s` is dir.", dir)
+		}
+	}
+	for _, file := range files {
+		if IsDir(file) {
+			t.Errorf("`%s` is not dir.", file)
+		}
 	}
 }
